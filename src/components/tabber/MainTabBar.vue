02097bd0fa6873de4e5a560a0982d1f8c7cab3a3
xiansen-bai <57938177+xiansen-bai@users.noreply.github.com>
2019-12-28 18:47:01 +0800

    Add files via upload

---
+++ src/components/tabber/MainTabBar.vue
@@ -0,0 +1,41 @@
+<template>
+	<tab-bar>
+			<tab-bar-item path="/home" activeColor="#F6D95F">
+				<img slot="item-icon" src="../../assets/img/tabbar/home_page.svg" alt="home">
+				<img slot="item-icon-active" src="../../assets/img/tabbar/home_page1.svg" alt="home">
+				<div slot="item-text">首页</div>
+			</tab-bar-item>
+			<tab-bar-item path="/category" activeColor="#D4237A">
+				<img slot="item-icon" src="../../assets/img/tabbar/classification.svg" alt="home">
+				<img slot="item-icon-active" src="../../assets/img/tabbar/classification1.svg" alt="home">
+				<div slot="item-text">分类</div>
+			</tab-bar-item>
+			<tab-bar-item path="/shopcart" activeColor="#D4237A">
+				<img slot="item-icon" src="../../assets/img/tabbar/Shopping_Cart.svg" alt="home">
+				<img slot="item-icon-active" src="../../assets/img/tabbar/Shopping_Cart1.svg" alt="home">
+				<div slot="item-text">购物车</div>
+			</tab-bar-item>
+			<tab-bar-item path="/profile" activeColor="#D4237A">
+				<img slot="item-icon" src="../../assets/img/tabbar/user.svg" alt="home">
+				<img slot="item-icon-active" src="../../assets/img/tabbar/user1.svg" alt="home">
+				<div slot="item-text">我的</div>
+			</tab-bar-item>
+	</tab-bar>
+</template>
+
+<script>
+	import TabBar from './tabbar.vue'
+	import TabBarItem from './tabbaritem.vue'
+	
+	export default{
+		name:'MainTabBar',
+		components:{
+			TabBar,
+			TabBarItem
+		}
+	}
+</script>
+
+<style>
+	
+</style>
---
+++ src/components/tabber/tabbaritem.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="tab-bar-item" @click="itemClick"> 
+		<div v-if=" !isActive "><slot name="item-icon"></slot></div>
+		<div v-else><slot name="item-icon-active"></slot></div>
+		<div :style="activeStyle">
+			<slot name="item-text"></slot>
+		</div>
+		
+		<!-- <img src="../../assets/img/tabbar/home_page.svg"/>
+		<div>首页</div> -->
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"TabBarItem",
+		props:{
+			path:String,
+			activeColor:{
+				type:String,
+				default:'#D4237A'
+			}
+		},
+		data(){
+			return{
+				//isActive:true
+			}
+		},
+		//计算属性
+		computed:{
+			isActive(){
+				// /home ->item1(/home) = true
+				// /home ->item1(/category) = false
+				// /home ->item1(/shopcart) = true
+				// /home ->item1(/profile) = true
+				return this.$route.path.indexOf(this.path) !== -1
+			},
+			activeStyle(){
+				return this.isActive ? {color: this.activeColor} : {}
+			},
+		},
+		methods:{
+			itemClick(){
+				// console.log("itemClick");
+				this.$router.replace(this.path)
+			}
+		}
+	}
+</script>
+
+<style>
+	.tab-bar-item{
+		flex: 1;
+		height: 49px;
+		text-align: center;
+		font-size: 14px;
+	}
+	.tab-bar-item img{
+		width: 24px;
+		height: 24px;
+		margin-top: 3px;
+		vertical-align: middle;
+	}
+</style>
